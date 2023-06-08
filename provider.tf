38f50d931862562e2ef2b48d661a439b74eaeb8c
Dilshad-Rangrej <133975393+Dilshad-Rangrej@users.noreply.github.com>
2023-06-08 15:31:34 +0530

    Update provider.tf
    in this change i removed the access and secret key from provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region	= "ap-south-1"
-  access_key	= "AKIAVFXTAJYQA4IEMMW6"
-  secret_key	= "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"
+  access_key	= ""
+  secret_key	= ""
 }
 
